196561b553a599406c8ec04a06e644acd586d4eb
Jonny-programmer <76569574+Jonny-programmer@users.noreply.github.com>
2022-07-20 07:31:19 +0300

    20 Jul - backgrounds are able to change

--- main.py
+++ main.py
@@ -32,6 +32,20 @@ def update_operator_num(order_num, cur, conn):
     conn.commit()
 
 
+def minus_one_operator(order_num, cur, conn):
+    table_op_quant = cur.execute("""SELECT operators_quant FROM control_table WHERE order_num=?""", (order_num,)).fetchone()
+    if not table_op_quant:
+        cur.execute("INSERT INTO control_table (order_num, operators_quant) VALUES (?, 0)", (order_num,))
+        table_op_quant = 0
+    else:
+        table_op_quant = table_op_quant[0] - 1
+    cur.execute("""UPDATE control_table SET operators_quant=? WHERE order_num=?""", (table_op_quant, order_num,))
+    conn.commit()
+    call_string = str(f"ALTER TABLE order_{order_num} DROP COLUMN operator_{table_op_quant}")
+    cur.execute(call_string)
+    conn.commit()
+
+
 def reset_operator_num(order_num, cur, conn):
     # Пока не используется, можно с его помощью сбросить прогресс всех операторов
     cur.execute("""UPDATE control_table SET operators_quant=0 WHERE order_num=?""", (order_num,))
@@ -127,9 +141,9 @@ class SelectEnterType(QMainWindow):
         uic.loadUi(resource_path("templates/SelectEnterTypeTemplate.ui"), self)
         self.setCentralWidget(self.gridWidget)
         # Установка фона
-        palette = QPalette()
-        palette.setBrush(QPalette.Background, QBrush(QPixmap(resource_path("backgrounds/img.png"))))
-        self.setPalette(palette)
+        # palette = QPalette()
+        # palette.setBrush(QPalette.Background, QBrush(QPixmap(resource_path("backgrounds/img.png"))))
+        # self.setPalette(palette)
 
         self.opt_1_btn.clicked.connect(lambda: self.move_on("opt_multiple"))
         self.opt_2_btn.clicked.connect(lambda: self.move_on("opt_db"))
@@ -173,6 +187,12 @@ class EnterSNListWindow(QMainWindow):
         self.setPalette(palette)
 
         self.modify_table()
+        # Настройка крутилки
+        self.dial.setMinimum(1)
+        self.dial.setMaximum(7)
+        value = 5
+        self.dial.setValue(value)
+        self.dial.valueChanged.connect(lambda: self.change_background())
         # Подключение кнопок
         self.save_btn.clicked.connect(self.save_table)
         self.return_home_btn.clicked.connect(self.return_back)
@@ -263,9 +283,6 @@ class EnterSNListWindow(QMainWindow):
                 real_operator_num = get_operator_num(self.order_num, self.local_cur, self.local_con)
                 self.dialog = QDialog(self)
                 uic.loadUi(resource_path("templates/MultiOperatorTemplate.ui"), self.dialog)
-                text = "Введите,  пожалуйста,  список серийных номеров,  разделенных \nпереносом строки." \
-                       "Вы можете использовать для этого\nсканер штрих-кодов."
-                self.dialog.label.setText(text)
                 self.dialog.comment_label.setText(f"Оператор номер {get_operator_num(self.order_num, self.local_cur, self.local_con)}")
                 if self.dialog.exec():
                     op_column_name = f"operator_{real_operator_num}"
@@ -348,7 +365,10 @@ class EnterSNListWindow(QMainWindow):
                             msg.setInformativeText('У вас не совпало ни одного номера с предыдущим оператором!')
                             msg.setWindowTitle("Warning")
                             msg.exec_()
+                            minus_one_operator(self.order_num, self.local_cur, self.local_con)
+                            self.return_back()
                 else:
+                    minus_one_operator(self.order_num, self.local_cur, self.local_con)
                     self.return_back()
 
         # Для надежности
@@ -434,6 +454,24 @@ class EnterSNListWindow(QMainWindow):
                 self.model.setItem(item.row(), 1, new_item)
                 self.model.setItem(item.row(), 2, code_found)
             self.scan_field.clear()
+
+    def change_background(self):
+        # Изменение фона
+        value = int(self.dial.value())
+        if value == 7:
+            self.label.setText("<p style='color: rgb(250, 250, 250)' align='left'>Поле для сканирования</p>")
+            self.return_home_btn.setStyleSheet("text-align: left; color: white; text-decoration:underline; "
+                                               "background-color: transparent; font-weight: italic;")
+        elif value == 3:
+            self.return_home_btn.setStyleSheet("text-align: left; color: white; text-decoration:underline; "
+                                               "background-color: transparent; font-weight: italic;")
+        else:
+            self.label.setText("<p style='color: rgb(0, 0, 0)' align='left'>Поле для сканирования</p>")
+            self.return_home_btn.setStyleSheet("text-align:left; color: rgb(10, 87, 182); background-color: transparent; "
+                                               "text-decoration: underline; font-weight: italic;")
+        palette = QPalette()
+        palette.setBrush(QPalette.Background, QBrush(QPixmap(resource_path(f"backgrounds/vector_{value}.jpeg"))))
+        self.setPalette(palette)
 
     def save_table(self):
         try:
